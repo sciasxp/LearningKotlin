e3a01ff03d327ca3ba22ec0860a7ee4038a432c4
Luciano Nunes <sciasxp@gmail.com>
2021-02-23 10:44:01 -0300

    DiceRoller: code optimization

--- DiceRoller/app/src/main/java/com/bardonunes/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/bardonunes/diceroller/MainActivity.kt
@@ -8,6 +8,8 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,20 +18,19 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
+
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
-        val diceImage: ImageView = findViewById(R.id.dice_image)
-
         val randomInt = Random().nextInt(6) + 1
         val drawableResource = when (randomInt) {
-            1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
             4 -> R.drawable.dice_4
             5 -> R.drawable.dice_5
             6 -> R.drawable.dice_6
-            else -> R.drawable.empty_dice
+            else -> R.drawable.dice_1
         }
         diceImage.setImageResource(drawableResource)
     }
