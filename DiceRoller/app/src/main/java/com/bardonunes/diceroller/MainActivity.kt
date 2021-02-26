0559f0af78e972ccce8b64ae5f6aa871a451eda2
Luciano Nunes <sciasxp@gmail.com>
2021-02-26 10:35:09 -0300

    Add about me project

--- DiceRoller/app/src/main/java/com/bardonunes/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/bardonunes/diceroller/MainActivity.kt
@@ -25,12 +25,13 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
         val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
             4 -> R.drawable.dice_4
             5 -> R.drawable.dice_5
             6 -> R.drawable.dice_6
-            else -> R.drawable.dice_1
+            else -> R.drawable.empty_dice
         }
         diceImage.setImageResource(drawableResource)
     }
